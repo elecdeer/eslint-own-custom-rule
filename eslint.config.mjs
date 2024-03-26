ea0d68faa9080af6c8bb32d0fed077ed5a017a90
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-03-27 01:37:12 +0900

    chore: セットアップ

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import eslint from "@eslint/js";
+import prettierConfigPlugin from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import typescriptEslint from "typescript-eslint";
+
+export default typescriptEslint.config(
+  {
+    ignores: ["dist", "eslint.config.mjs", "vite.config.ts"],
+  },
+  eslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.es2020,
+        ...globals.browser,
+      },
+    },
+  },
+  ...typescriptEslint.configs.recommendedTypeChecked,
+  ...typescriptEslint.configs.stylisticTypeChecked,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  prettierConfigPlugin,
+);
